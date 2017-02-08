fd2632bc928ed10a11a2f299503a815924270930
Shawn Hubbart <shawn.hubbart@gmail.com>
2017-02-07 17:12:49 -0800

    ObjectPool Iterator has been completed

--- ParticleFactory/ObjectPool.h
+++ ParticleFactory/ObjectPool.h
@@ -31,4 +31,26 @@ public:
 			m_data[i].next = i + 1;
 		}
 	}
+
+	class iterator
+	{
+		ObjectPool *m_ref;
+		size_t m_idx;
+		friend class ObjectPool;
+		iterator(ObjectPool *a_ref = nullptr, size_t a_idx = 0) : m_ref(a_ref), m_idx(a_idx) {}
+	public:
+		iterator() : m_ref(nullptr), m_idx(0) {}
+		particle &operator*() {return m_ref->m_data[m_idx].data; }
+		particle *operator->() { return &m_ref->m_data[m_idx].data; }
+		const particle &operator*() const { return m_ref->m_data[m_idx].data; }
+		const particle *operator->() const { return &m_ref->m_data[m_idx].data; }
+		iterator &operator++() { m_idx = m_ref->m_data[m_idx].next;  return *this; }
+		iterator operator++(int) { auto that = *this; operator++(); return that; }
+
+		bool operator==(const iterator &O) const { return m_ref == O.m_ref && m_idx == O.m_idx; }
+		bool operator!=(const iterator &O) const { return !(*this == O); }
+
+		operator bool() const {m_ref != nullptr && m_idx < m_ref->m_size && !m_ref->m_data[m_idx].open; }
+	};
+
 };
